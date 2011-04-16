ead37bfee1d16f3ec1a50025a6acdd01253475d7
jfranklin773 <jfranklin773@abd14230-2542-0d74-124b-4d8f9b872d06>
2011-04-16 20:29:56 +0000

    fixed engine

--- trunk/TankUniverse/src/edu/purdue/tanks/universe/game/GameEngine.java
+++ trunk/TankUniverse/src/edu/purdue/tanks/universe/game/GameEngine.java
@@ -10,7 +10,7 @@ public class GameEngine extends Thread
 	 * constructor for GameEngine
 	 * @param gameObjects
 	 */
-	ArrayList <GameObject> gameObjects;
+	ArrayList<GameObject> gameObjects;
 	public GameEngine(ArrayList <GameObject> gameObjects)
 	{
 		this.gameObjects = gameObjects;
@@ -20,13 +20,16 @@ public class GameEngine extends Thread
 	 * The main game engine, updates the positions of all objects in GameObjects
 	 */
 	@Override
-	public void run()
+	public synchronized void run()
 	{
 		while(!done) // outer game loop
 		{
 			try{Thread.sleep(30);}catch(Exception e){}
 			for(GameObject g:gameObjects)
 			{
+				if(g.needsToBeRemoved) {
+					gameObjects.remove(g);
+				}					
 				double time = System.currentTimeMillis();
 				g.update(time);
 			}
