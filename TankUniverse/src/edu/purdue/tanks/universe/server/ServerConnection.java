d169bb312dc396d398973491bddfb519e90c8274
jfranklin773@gmail.com <jfranklin773@gmail.com@abd14230-2542-0d74-124b-4d8f9b872d06>
2011-09-28 00:21:46 +0000

    Server connection abstraction interface.

---
+++ TankUniverse/src/edu/purdue/tanks/universe/server/ServerConnection.java
@@ -0,0 +1,9 @@
+package edu.purdue.tanks.universe.server;
+
+import java.io.IOException;
+
+
+public interface ServerConnection {
+	public void send(String obj) throws IOException;
+	public String receive() throws IOException;
+}
