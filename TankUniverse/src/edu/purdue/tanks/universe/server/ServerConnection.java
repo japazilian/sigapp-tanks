5abb8526328efa48f093f260f70a236caebba518
jfranklin773@gmail.com <jfranklin773@gmail.com@abd14230-2542-0d74-124b-4d8f9b872d06>
2011-10-25 23:40:40 +0000

    add connectToHost method

--- TankUniverse/src/edu/purdue/tanks/universe/server/ServerConnection.java
+++ TankUniverse/src/edu/purdue/tanks/universe/server/ServerConnection.java
@@ -2,8 +2,8 @@ package edu.purdue.tanks.universe.server;
 
 import java.io.IOException;
 
-
 public interface ServerConnection {
 	public void send(String obj) throws IOException;
+	public void connectToHost() throws IOException;
 	public String receive() throws IOException;
 }
