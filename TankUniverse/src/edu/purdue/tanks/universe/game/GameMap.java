4aae9a85549f38e2c7b1e18bea25dfc01321c658
wthenrichs@gmail.com <wthenrichs@gmail.com@abd14230-2542-0d74-124b-4d8f9b872d06>
2011-10-26 00:50:09 +0000

    After meeting on 10/25/2011, Creating game logic in Game.java, PlayerStats.java, Round.java, and GameMap.java

---
+++ TankUniverse/src/edu/purdue/tanks/universe/game/GameMap.java
@@ -0,0 +1,7 @@
+package edu.purdue.tanks.universe.game;
+
+public class GameMap {
+
+	public String mapId;
+	
+}
--- TankUniverse/src/edu/purdue/tanks/universe/game/Round.java
+++ TankUniverse/src/edu/purdue/tanks/universe/game/Round.java
@@ -1,28 +1,30 @@
 package edu.purdue.tanks.universe.game;
 
 import java.util.HashMap;
-import java.util.List;
+
 
 public class Round {
 	
 	// this needs to be a hashmap because we need to give a pId and get the playerStats object back
-	public HashMap<Integer, PlayerStats> pStats; 
-	public int gameTime;
-	public boolean roundOver; // False is not over, True is round over
-	public boolean playerAlive; // False is player dead, True is player alive
+	public HashMap<Integer, PlayerStats> pStats;
+	public HashMap<Integer, PlayerStats> alivePlayers;
+	public int roundId;
+	public int roundTime;
+	public boolean isRoundOver; // False is not over, True is round over
+	public GameMap roundMap;
 	
-	public Round(List<PlayerStats> playerList, int gameTime){
-		for(PlayerStats player : playerList){
-			pStats.put(player.playerId, player);
-		}
-		this.gameTime = gameTime;
+	public Round(HashMap<Integer, PlayerStats> playerList,int roundId, int roundTime, 
+				 String roundMapId){
+		this.pStats = playerList;
+		this.alivePlayers = playerList;
+		this.roundId = roundId;
+		this.roundTime = roundTime;
+		this.roundMap.mapId = roundMapId;
 	}
 	
-	public void UpdatePlayer(int playerId, int addPoints, int addKills){
+	/*public void UpdatePlayer(int playerId, int addPoints, int addKills){
 		if(pStats.containsKey(playerId)){
 			//This means that the player we are trying to update is in the pStats Hashmap
-			int currentPoints = pStats.get(playerId).points;
-			int currentKills = pStats.get(playerId).kills;
 			pStats.get(playerId).points += addPoints;
 			pStats.get(playerId).kills += addKills;
 		} else {
@@ -33,4 +35,5 @@ public class Round {
 				// hashmap will go here
 		}
 	}
+	*/
 }
