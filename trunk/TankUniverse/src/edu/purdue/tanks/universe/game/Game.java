02de0d5100d573ed5c716972f8944bb9fb7aaa98
wthenrichs@gmail.com <wthenrichs@gmail.com@abd14230-2542-0d74-124b-4d8f9b872d06>
2011-10-26 00:50:09 +0000

    After meeting on 10/25/2011, Creating game logic in Game.java, PlayerStats.java, Round.java, and GameMap.java

--- trunk/TankUniverse/src/edu/purdue/tanks/universe/game/Game.java
+++ trunk/TankUniverse/src/edu/purdue/tanks/universe/game/Game.java
@@ -2,18 +2,58 @@ package edu.purdue.tanks.universe.game;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Game {
 	
+	public static final int HIT_POINTS = 10;
+	public static final int KILL_POINTS = 100;
+	
 	public HashMap<Integer, PlayerStats> pStats;
 	public List<Round> rounds;
-	public List<Map> maps; //Map class that will contain File
-	
-	public Game(HashMap<Integer, PlayerStats> playerStats, List<Round> rounds, List<Map> maps)
-	{
-		pStats = playerStats;
-		this.rounds = rounds;
-		this.maps = maps;
+	public boolean isGameOver;
+	public Round currentRound;
+		
+	public Game(List<Integer> playerIds, List<Integer> roundIds,int roundTime, List<String> mapIds){
+		for(Integer playerId : playerIds){
+			// This populates the pStats Hashmap with playerId and PlayerStats Object
+			pStats.put(playerId, new PlayerStats(playerId));
+		}
+		for(int i=0; i < roundIds.size(); i++){
+			// This populates the rounds array with correct round data
+			// Each round has the playerStats array, roundIds,roundTime, and the mapId
+			rounds.add(new Round(pStats,roundIds.get(i),roundTime,mapIds.get(i)));
+		}
 	}
+	
+	public void startGame(){
+		if(rounds.isEmpty()){
+			//throw new NoRoundsInitializedYetException
+		}
+		currentRound = rounds.get(0);
+		isGameOver = false;
+	}
+	
+	public void endGame(){
+		isGameOver = true;
+	}
+	
+	public void playerHit(int playerIdHit, int bulletPlayerId){
+		PlayerStats playerWhoGotHit = currentRound.pStats.get(playerIdHit);
+		PlayerStats playerWhoShot = currentRound.pStats.get(bulletPlayerId);
+		playerWhoGotHit.decreasePlayerHealth();
+		playerWhoShot.increasePoints(HIT_POINTS);
+		if(!playerWhoGotHit.isplayerAlive){
+			playerWhoShot.addKill();
+			playerWhoShot.increasePoints(KILL_POINTS);
+		}
+	}
+	
+	public void playerKilled(int playerId){
+		PlayerStats playerKilled = currentRound.pStats.get(playerId);
+		playerKilled.isplayerAlive = false;
+		
+	}
+	
+	
 }
--- trunk/TankUniverse/src/edu/purdue/tanks/universe/game/PlayerStats.java
+++ trunk/TankUniverse/src/edu/purdue/tanks/universe/game/PlayerStats.java
@@ -2,13 +2,47 @@ package edu.purdue.tanks.universe.game;
 
 public class PlayerStats {
 	
-	public int points;
-	public int kills;
+	private int playerHealth;
+	
+	private int points;
+	private int kills;
 	public int playerId;
+	public boolean isplayerAlive; // False is player dead, True is player alive
 	
 	public PlayerStats(int p_Id){
 		playerId = p_Id;
 		points = 0;
 		kills = 0;
-	}	
+	}
+
+	public void initializePlayerHealth(int health){
+		if(health <= 0){
+			//throw new NegativeHealthException();
+			// I am not sure if I am allowed to throw Exception here
+			// Is it better to create a new Exception class or something else????
+		}
+		this.playerHealth = health;
+	}
+	
+	public void decreasePlayerHealth(){
+		if(!isplayerAlive){
+			//throw new PlayerDeadException()
+			//same issue as in initializePlayerHealth method
+		}
+		this.playerHealth--;
+		if(playerHealth == 0){
+			isplayerAlive = false;
+			
+		}
+	}
+	
+	public void increasePoints(int pointsToAdd){
+		this.points += pointsToAdd;
+	}
+	
+	public void addKill(){
+		this.kills++;
+	}
+	
+	
 }
